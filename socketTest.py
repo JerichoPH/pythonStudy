e117d88f0aaee13b2377fa4207b105fd6356af68
jericho <jericho_ph@qq.com>
2022-11-15 18:28:46 +0800

    m

--- socketTest.py
+++ socketTest.py
@@ -1,5 +1,5 @@
 import socket
 
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-a = socket_client.connect(('192.168.0.104', 8001))
+a = socket_client.connect(('127.0.0.1', 8001))
 print('-' * 10, a)
