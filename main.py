dfc7ba0ab88c40cb0e087253b9868fe3c360348b
jericho <jericho_ph@qq.com>
2022-11-24 16:31:19 +0800

    新增单例模式元类

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-class SingletonType(type):
-    _instance = None
-
-    def __call__(self, *args, **kwargs):
-        if not self._instance:
-            self._instance = self.__new__(self)
-
-        return self._instance
+from singletonType import SingletonType
 
 
 class SingletonCls(object, metaclass=SingletonType):
